e0d929324d3ba721b7963e5887890c482e5ad650
Sholkhan <muhammadsholkhan896@gmail.com>
2024-07-08 00:41:19 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -9,5 +9,13 @@ package hitung;
  * @author adria
  */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat object
+        Matematika sholkhan = new Matematika(6,8);
+        
+        System.out.println("hasil penjumlahan: "+sholkhan.setPenjumlahan());
+        System.out.println("hasil pengurangan: "+sholkhan.setPengurangan());
+        System.out.println("hasil perkalian: "+sholkhan.setPerkalian());
+        System.out.println("hasil pembagian: "+sholkhan.setPembagian());
+    }
 }
